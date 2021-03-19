80ff95e2f58add7c81494704a5c443daa07900cb
Paul Ly <paul.ly@live.ca>
2021-03-18 20:33:38 -0400

    I hope this works

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+
 const Navbar = ()  => {
     return (
         <div className="navbar">
--- src/components/pages/ContactKeeper.js
+++ src/components/pages/ContactKeeper.js
@@ -6,22 +6,10 @@ import Carousel from '@brainhubeu/react-carousel';
 import '@brainhubeu/react-carousel/lib/style.css';
 const ContactKeeper = () =>  {
 
-
         return (      
             <React.Fragment>
                 <div className="container p-2 ">
-                    <Carousel
-                        plugins={[
-                            'arrows',
-                            'centered',
-                            'fastSwipe',
-                            'infinite'
-                        ]}
-                    >
-                    <img src={ContactKeepers} width="100%"/>
-                    <img src={ContactKeepers2} width="100%"/>      
-                    <img src={ContactKeepers3} width="100%"/>       
-                    </Carousel>
+                    
                     <p className ="text-center text-primary p-2">
                     This is my first full stack application created for learning the back-end. 
                     I used postman for creating my API and use MongoDB to serve as my Cloud Database. You can view this project here
